8f93ab8887380cec464a96c106ac250faf1e74ad
ueyamamasashi <ueyamaza@mctv.ne.jp>
2021-02-26 07:11:45 +0900

    question:aysnc-await in ejs.file

--- routes/index.js
+++ routes/index.js
@@ -10,14 +10,10 @@ router.get('/', async (req, res, next)=> {
     const ans = json[i]['correctAns'];
     corrAnsArray.push(ans);
   }
-  //index.ejsでfetchの非同期処理を実施する関数を定義したい
-  const sample = async ()=>{
-    const apiPromise = api();
-    const apiAwait = await api();
-  }
+  
   res.render('index', {
     corrAnsArray: corrAnsArray,
-    api: sample(),//async-await関数を作成しfetchするまでindex.ejsを待たせたい
+    api: api(),//async-await関数を作成しfetchするまでindex.ejsを待たせたい
     json: json,
 
   });
